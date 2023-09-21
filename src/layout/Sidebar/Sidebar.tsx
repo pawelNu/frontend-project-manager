dee3c6b72d940d2b9d04f6085e3ca220519ef035
pawelNu <you@example.com>
2023-09-21 09:53:46 +0200

    Sidebar structure

--- src/layout/Sidebar/Sidebar.tsx
+++ src/layout/Sidebar/Sidebar.tsx
@@ -1,28 +1,97 @@
 // https://getbootstrap.com/docs/5.3/components/offcanvas/
+// https://www.cssscript.com/responsive-sidebar-bootstrap-offcanvas/ -> 2. The HTML structure for the offcanvas sidebar.
 
 export const Sidebar = () => {
     return (
         <div>
             <div className="offcanvas offcanvas-start" tabIndex={-1} id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
                 <div className="offcanvas-header">
-                    <h5 className="offcanvas-title" id="offcanvasExampleLabel">Menu</h5>
-                    <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                <div className="offcanvas-header border-bottom">
+                    <a href="/" className="d-flex align-items-center text-decoration-none offcanvas-title d-sm-block">
+                        <h5>
+                            <i className="bi bi-chat-right-text-fill"></i>
+                            Sidebar
+                        </h5>
+                    </a>
+                    
                 </div>
-                <div className="offcanvas-body">
-                    <div className="dropdown mt-3">
-                        <a className="btn btn-outline-primary" href="/#" role="button">
-                            Projects
-                        </a>
-                        <div className="dropdown-divider"></div>
-                        <button className="btn btn-outline-primary dropdown-toggle mt-2" type="button" data-bs-toggle="dropdown">
-                            Dropdown button2
-                        </button>
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="/#">Action2</a></li>
-                            <li><a className="dropdown-item" href="/#">Another action2</a></li>
-                            <li><a className="dropdown-item" href="/#">Something else here2</a></li>
-                        </ul>
-                    </div>
+                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div className="offcanvas-body px-0">
+                    <ul className="list-unstyled ps-0">
+                        <li className="mb-1">
+                            <button
+                                className="btn btn-toggle align-items-center rounded"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#projects-collapse"
+                                aria-expanded="true"
+                            >
+                                Projects
+                            </button>
+                            <div className="collapse show" id="projects-collapse">
+                                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="/#" className="rounded">Overview</a></li>
+                                    <li><a href="/#" className="rounded">Updates</a></li>
+                                    <li><a href="/#" className="rounded">Reports</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="mb-1">
+                            <button
+                                className="btn btn-toggle align-items-center rounded collapsed"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#users-collapse"
+                                aria-expanded="false"
+                            >
+                                Users
+                            </button>
+                            <div className="collapse" id="users-collapse">
+                                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="/#" className="rounded">Overview</a></li>
+                                    <li><a href="/#" className="rounded">Weekly</a></li>
+                                    <li><a href="/#" className="rounded">Monthly</a></li>
+                                    <li><a href="/#" className="rounded">Annually</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="mb-1">
+                            <button
+                                className="btn btn-toggle align-items-center rounded collapsed"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#orders-collapse"
+                                aria-expanded="false"
+                            >
+                                Orders
+                            </button>
+                            <div className="collapse" id="orders-collapse">
+                                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="/#" className="rounded">New</a></li>
+                                    <li><a href="/#" className="rounded">Processed</a></li>
+                                    <li><a href="/#" className="rounded">Shipped</a></li>
+                                    <li><a href="/#" className="rounded">Returned</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="border-top my-3"></li>
+                        <li className="mb-1">
+                            <button
+                                className="btn btn-toggle align-items-center rounded collapsed"
+                                data-bs-toggle="collapse"
+                                data-bs-target="#account-collapse"
+                                aria-expanded="false"
+                            >
+                                Account
+                            </button>
+                            <div className="collapse" id="account-collapse">
+                                <ul className="btn-toggle-nav list-unstyled fw-normal pb-1 small">
+                                    <li><a href="/#" className="rounded">New...</a></li>
+                                    <li><a href="/#" className="rounded">Profile</a></li>
+                                    <li><a href="/#" className="rounded">Settings</a></li>
+                                    <li><a href="/#" className="rounded">Sign out</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
