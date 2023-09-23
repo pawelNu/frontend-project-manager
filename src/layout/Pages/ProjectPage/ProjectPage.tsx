9cc72e39b1d8714aad178c39d84bfbbdbb9830e4
pawelNu <you@example.com>
2023-09-23 20:26:41 +0200

    Added table styling for ProjectPage and UserPage

--- src/layout/Pages/ProjectPage/ProjectPage.tsx
+++ src/layout/Pages/ProjectPage/ProjectPage.tsx
@@ -1,6 +1,7 @@
+import { useEffect, useState } from 'react';
 import { projectData } from '../../../data/projectData'
+import { Project } from '../../../interface/Project';
 
-// TODO display all projects in table
 // TODO display project searched by name
 // TODO add new project
 // TODO update project
@@ -8,17 +9,50 @@ import { projectData } from '../../../data/projectData'
 
 export const ProjectPage = () => {
 
+    // const [projects, setProjects] = useState<Project[]>([])
+
+    // useEffect
+
     return (
         <div>
             <div>
                 <h1>TODO Project Page</h1>
             </div>
-            <div>
-                <ul>
-                    {projectData.map((project) => (
-                        <li key={project.id}>{project.name}</li>
-                    ))}
-                </ul>
+            <div className='container'>
+                {/* https://getbootstrap.com/docs/5.3/content/tables/#table-borders */}
+                {/* https://getbootstrap.com/docs/5.3/content/tables/#hoverable-rows */}
+                {/* https://getbootstrap.com/docs/5.3/content/tables/#small-tables */}
+                <table className="table table-bordered table-hover table-sm border shadow">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Project name</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-group-divider">
+                        {projectData.map((project, index) => (
+                            <tr>
+                                <th scope="row">{index + 1}</th>
+                                <td>{project.name}</td>
+                                <td>
+                                    {/* https://getbootstrap.com/docs/5.3/components/dropdowns/#single-button */}
+                                    <div className="dropdown">
+                                        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Action
+                                        </button>
+                                        <ul className="dropdown-menu">
+                                            <li><a className="dropdown-item bg-info" href="#">View</a></li>
+                                            <li><a className="dropdown-item bg-warning" href="#">Update</a></li>
+                                            <li><hr className="dropdown-divider"></hr></li>
+                                            <li><a className="dropdown-item bg-danger text-white" href="#">Delete</a></li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
     );
--- src/layout/Pages/UserPage/UsersPage.tsx
+++ src/layout/Pages/UserPage/UsersPage.tsx
@@ -1,24 +1,54 @@
 import { userData } from '../../../data/userData'
 
-// TODO display all users in table
 // TODO display user searched by name
 // TODO add new user
 // TODO update user
 // TODO delete user
 
 export const UsersPage = () => {
-    return(
+
+    return (
         <div>
             <div>
-            <h1>TODO Users Page</h1>
+                <h1>TODO User Page</h1>
             </div>
-            <div>
-                <ul>
-                    {userData.map((user) => (
-                        <li key={user.id}>{user.firstName} {user.lastName} {user.email}</li>
-                    ))}
-                </ul>
+            <div className='container'>
+                {/* https://getbootstrap.com/docs/5.3/content/tables/#table-borders */}
+                {/* https://getbootstrap.com/docs/5.3/content/tables/#hoverable-rows */}
+                <table className="table table-bordered table-hover table-sm border shadow">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">User name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody className="table-group-divider">
+                        {userData.map((user, index) => (
+                            <tr>
+                                <th scope="row">{index + 1}</th>
+                                <td>{user.firstName} {user.lastName}</td>
+                                <td>{user.email}</td>
+                                <td>
+                                    {/* https://getbootstrap.com/docs/5.3/components/dropdowns/#single-button */}
+                                    <div className="dropdown">
+                                        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                            Action
+                                        </button>
+                                        <ul className="dropdown-menu">
+                                            <li><a className="dropdown-item bg-info" href="#">View</a></li>
+                                            <li><a className="dropdown-item bg-warning" href="#">Update</a></li>
+                                            <li><hr className="dropdown-divider"></hr></li>
+                                            <li><a className="dropdown-item bg-danger text-white" href="#">Delete</a></li>
+                                        </ul>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
             </div>
         </div>
-    )
-}
+    );
+};
