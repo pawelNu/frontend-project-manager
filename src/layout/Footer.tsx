619222d250153bb9b9962e9a04b2ead5de526efb
pawelNu <pnurzynski17@gmail.com>
2023-11-06 19:08:28 +0100

    feat (45): create footer

---
+++ src/layout/Footer.tsx
@@ -0,0 +1,13 @@
+import "./../static/styles/Footer.css";
+
+export const Footer = () => {
+    return (
+        <div className="card">
+            <div className="card-body">
+                <p className="card-text footer text-center">
+                    GitHub 2023 - Designed and implemented by @pawelNu
+                </p>
+            </div>
+        </div>
+    );
+};
