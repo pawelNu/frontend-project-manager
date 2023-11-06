0de210dd47f34f528458f49063bdbd66da096962
pawelNu <pnurzynski17@gmail.com>
2023-11-06 19:08:28 +0100

    feat (45): create footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Navbar } from "./layout/Navbar";
+import { Footer } from "./layout/Footer";
 
 export const App = () => {
     return (
         <>
             <Navbar />
+            <Footer />
         </>
     );
 };
