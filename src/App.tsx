ce783330a49608ce6bdda0e160f688fc289fc675
pawelNu <pnurzynski17@gmail.com>
2023-11-06 19:00:43 +0100

    feat (43): create navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import { Navbar } from "./layout/Navbar";
 
 export const App = () => {
     return (
-        <div>
-            <h1>Project Manager</h1>
-        </div>
+        <>
+            <Navbar />
+        </>
     );
 };
