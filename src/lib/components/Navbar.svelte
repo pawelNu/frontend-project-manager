cc218c5371530a2d149512b844d3979986ed1070
pawelNu <93542936+pawelNu@users.noreply.github.com>
2025-04-04 11:46:03 +0200

    Feat 62 add main page (#63)
    * refactor: removed unnecessary file
    
    * feat: added franken-ui, navbar and themes
    
    * style: code format
    
    * refactor: added TODOs
    
    * feat: added footer
    
    * feat: added main menu
    
    * feat: added example data

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import MainMenu2 from './MainMenu.svelte';
+
+  const links = [
+    { href: '/', label: 'Home' },
+    { href: '/about', label: 'About' }
+  ];
+</script>
+
+<!-- <nav class="navbar">
+  {#each links as link}
+    <a href={link.href} class={page.url.pathname.startsWith(link.href) ? 'active' : ''}>
+      {link.label}
+    </a>
+  {/each}
+</nav> -->
+
+<ul class="uk-tab navbar-font" data-uk-tab>
+  <!-- <li class="uk-active"><a href="/">Active</a></li> -->
+  <li class="uk-active">
+    <!-- <MainMenu /> -->
+    <MainMenu2 />
+  </li>
+  <li><a href="/">Item</a></li>
+  <li>
+    <a href="/">
+      <span class="mr-2">More</span>
+      <uk-icon icon="chevron-down"></uk-icon>
+    </a>
+    <div class="uk-drop min-w-52" data-uk-dropdown="mode: click">
+      <ul class="uk-nav uk-dropdown-nav">
+        <li class="uk-active"><a href="/">Active</a></li>
+        <li><a href="/">Item</a></li>
+        <li class="uk-nav-header">Header</li>
+        <li><a href="/">Item</a></li>
+
+        <li class="uk-nav-divider"></li>
+        <li><a href="/">Item</a></li>
+      </ul>
+    </div>
+  </li>
+</ul>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Footer from '$lib/components/Footer.svelte';
+  import Navbar from '$lib/components/Navbar.svelte';
+</script>
+
+<Navbar />
+
+<div class="uk-container uk-container-expand m-10">
+  <slot />
+</div>
+
+<Footer />
