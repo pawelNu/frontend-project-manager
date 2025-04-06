788ee06c88712948df871a49293350314794dc39
pawelNu <93542936+pawelNu@users.noreply.github.com>
2025-04-06 14:42:08 +0200

    Feat 64 add all companies page (#65)
    * feat: added random data generation and pagination component
    
    * feat: added pagination
    
    * feat: added page size
    
    * feat: added page with single company
    
    * feat: added redirecting from main /companies to url with params

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,5 +1,16 @@
-export interface Item {
-  id: number;
-  name: string;
-  description: string;
-}
+export type ErrorResponse = {
+  message: string;
+  details?: string;
+};
+
+export type Result<T> = { success: true; data: T } | { success: false; error: ErrorResponse };
+
+export type PaginatedResponse<T> = {
+  first: number;
+  prev: number | null;
+  next: number | null;
+  last: number;
+  pages: number;
+  items: number;
+  data: T[];
+};
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,29 +1,4 @@
 <script lang="ts">
-  import { onMount } from 'svelte';
-  import type { Item } from '$lib/types';
-
-  let items: Item[] = [];
-
-  onMount(async () => {
-    const response = await fetch('http://localhost:5000/projects');
-    if (response.ok) {
-      items = (await response.json()) as Item[];
-    } else {
-      console.error('Błąd podczas ładowania danych:', response.status);
-    }
-  });
 </script>
 
-<h1>Lista Obiektów</h1>
-
-{#if items.length === 0}
-  <p>Ładowanie danych...</p>
-{:else}
-  <ul>
-    {#each items as item}
-      <li>
-        <strong>{item.name}</strong> - {item.description}
-      </li>
-    {/each}
-  </ul>
-{/if}
+<h1>Projects</h1>
