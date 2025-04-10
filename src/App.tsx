4458b7e47bcdfdcbf4b814295363c7d7b26ea923
pawelNu <pnurzynski17@gmail.com>
2025-04-10 17:45:21 +0200

    feat: added highlighting on the button in sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Navbar } from './components/layout/Navbar';
 import { Sidebar } from './components/layout/Sidebar';
 import { MainPage } from './components/pages/MainPage';
 
-function App() {
+export const App = () => {
     return (
         <>
             <Navbar />
@@ -16,6 +16,4 @@ function App() {
             <Footer />
         </>
     );
-}
-
-export default App;
+};
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,17 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { ThemeToggleButton } from './ThemeToggleButton';
 import { routes } from '../routes';
 
-export const Sidebar = () => {
-    type TSidebar = {
-        levelName: string;
-        subLevel: TSubLevel[];
-    };
+export type TSidebar = {
+    levelName: string;
+    subLevel: TSubLevel[];
+};
 
-    type TSubLevel = {
-        levelName: string;
-        linkTo: string;
-    };
+type TSubLevel = {
+    levelName: string;
+    linkTo: string;
+};
+
+export const Sidebar = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const currentPath = location.pathname;
 
     const sidebarElements: TSidebar[] = [
         {
@@ -43,6 +47,11 @@ export const Sidebar = () => {
         }
     };
 
+    const handleLinkClick = (link: string) => {
+        closeSidebar();
+        navigate(link);
+    };
+
     return (
         <div>
             <div className="offcanvas offcanvas-start" tabIndex={-1} id="sidebar" aria-labelledby="sidebarLabel">
@@ -50,7 +59,7 @@ export const Sidebar = () => {
                     <Link
                         to={routes.pages.main()}
                         className="d-flex align-items-center text-decoration-none offcanvas-title d-sm-block"
-                        onClick={closeSidebar}>
+                        onClick={() => handleLinkClick(routes.pages.main())}>
                         <h5>
                             <i className="bi bi-chat-right-text-fill"></i>
                             Main Page
@@ -60,38 +69,33 @@ export const Sidebar = () => {
                 </div>
                 <div className="offcanvas-body px-0">
                     <div className="accordion" id="sidebarMenu">
-                        {sidebarElements.map((element, index) => (
-                            <div key={element.levelName} className="accordion-item">
-                                <h2 className="accordion-header" id={element.levelName + 'Heading' + index}>
-                                    <button
-                                        className="accordion-button"
-                                        type="button"
-                                        data-bs-toggle="collapse"
-                                        data-bs-target={'#' + element.levelName + 'Collapse' + index}
-                                        aria-expanded="true"
-                                        aria-controls={element.levelName + 'Collapse' + index}>
-                                        {element.levelName}
-                                    </button>
-                                </h2>
-                                <div
-                                    id={element.levelName + 'Collapse' + index}
-                                    className="accordion-collapse collapse"
-                                    aria-labelledby={element.levelName + 'Heading' + index}
-                                    data-bs-parent="#sidebarMenu">
-                                    <div className="accordion-body">
-                                        <ul className="list-unstyled">
-                                            {element.subLevel.map((sublevel, subIndex) => (
-                                                <li key={subIndex}>
-                                                    <Link to={sublevel.linkTo} onClick={closeSidebar}>
-                                                        {sublevel.levelName}
-                                                    </Link>
-                                                </li>
-                                            ))}
-                                        </ul>
+                        {sidebarElements.map((element, index) => {
+                            const isOnPath = element.subLevel.some((subLink) => currentPath === subLink.linkTo);
+                            return (
+                                <div key={element.levelName} className="accordion-item">
+                                    <h2 className="accordion-header" id={element.levelName + 'Heading' + index}>
+                                        <button
+                                            className={`accordion-button ${isOnPath ? '' : ' collapsed'}`}
+                                            type="button"
+                                            data-bs-toggle="collapse"
+                                            data-bs-target={'#' + element.levelName + 'Collapse' + index}
+                                            aria-expanded={isOnPath ? 'true' : 'false'}
+                                            aria-controls={element.levelName + 'Collapse' + index}>
+                                            {element.levelName}
+                                        </button>
+                                    </h2>
+                                    <div
+                                        id={element.levelName + 'Collapse' + index}
+                                        className={`accordion-collapse collapse ${isOnPath ? 'show' : ''}`}
+                                        aria-labelledby={element.levelName + 'Heading' + index}
+                                        data-bs-parent="#sidebarMenu">
+                                        <div className="accordion-body">
+                                            {listElement(element, handleLinkClick, currentPath)}
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        ))}
+                            );
+                        })}
                         <ThemeToggleButton />
                     </div>
                 </div>
@@ -99,3 +103,36 @@ export const Sidebar = () => {
         </div>
     );
 };
+const listElement = (element: TSidebar, handleLinkClick: (link: string) => void, currentPath: string) => {
+    return (
+        <div className="list-group">
+            {element.subLevel.map((sublevel, subIndex) => {
+                const isCurrentPath = currentPath === sublevel.linkTo;
+                return (
+                    // <Link key={subIndex} to={sublevel.linkTo} onClick={() => handleLinkClick(sublevel.linkTo)}>
+                    <button
+                        key={subIndex}
+                        type="button"
+                        className={`list-group-item list-group-item-action rounded-0 ${isCurrentPath ? ' active' : ''}`}
+                        aria-current={isCurrentPath ? 'true' : 'false'}
+                        onClick={() => handleLinkClick(sublevel.linkTo)}>
+                        {sublevel.levelName}
+                    </button>
+                    // </Link>
+                );
+            })}
+        </div>
+    );
+};
+
+{
+    /* <div class="list-group">
+  <button type="button" class="list-group-item list-group-item-action active" aria-current="true">
+    The current button
+  </button>
+  <button type="button" class="list-group-item list-group-item-action">A second button item</button>
+  <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+  <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+  <button type="button" class="list-group-item list-group-item-action" disabled>A disabled button item</button>
+</div> */
+}
