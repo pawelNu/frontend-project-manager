3cb7e124bd3ecb2091ebb64d4451a589d94a1d74
pawelNu <pnurzynski17@gmail.com>
2025-04-12 17:21:10 +0200

    feat: added routes config

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Navbar } from './components/layout/Navbar';
 import { Sidebar } from './components/layout/Sidebar';
 import { MainPage } from './components/pages/MainPage';
 import { routes } from './components/routes';
+import { routeConfig } from './config/routes.config';
 
 export const App = () => {
     return (
@@ -13,6 +14,9 @@ export const App = () => {
             <Sidebar />
             <Routes>
                 <Route path={routes.pages.main()} element={<MainPage />} />
+                {routeConfig.map((route, index) => (
+                    <Route key={index} path={route.path} element={route.element} />
+                ))}
             </Routes>
             <Footer />
         </>
