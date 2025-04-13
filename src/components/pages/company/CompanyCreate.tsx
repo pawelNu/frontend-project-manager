57873c4cd50a9e60df5182666db3e4c709905df0
pawelNu <pnurzynski17@gmail.com>
2025-04-13 16:09:21 +0200

    docs: added link to forms docs

--- src/components/pages/company/CompanyCreate.tsx
+++ src/components/pages/company/CompanyCreate.tsx
@@ -1,5 +1,5 @@
 export const CompanyCreate = () => {
-    // TODO Add create company form
+    // TODO Add create company form https://getbootstrap.com/docs/5.3/forms/validation/
     return (
         <>
             <div className="container">
