6562081ed4b2f4a7ff8b00bc30a27b2e8094210f
pawelNu <pnurzynski17@gmail.com>
2025-04-13 14:10:30 +0200

    feat: added buttons before and after current page

--- src/components/common/Pagination.tsx
+++ src/components/common/Pagination.tsx
@@ -17,6 +17,7 @@ export type PageActions = {
 
 export const Pagination = ({ pagination, actions }: { pagination: PaginationType; actions: PageActions }) => {
     const [pageSize, setPageSize] = useState<number>(pagination.pageSize);
+    const pageBuffor = 2;
 
     const changePageNumberAndSize = (e: React.MouseEvent, num: number | null, size: number) => {
         e.preventDefault();
@@ -28,107 +29,101 @@ export const Pagination = ({ pagination, actions }: { pagination: PaginationType
 
     return (
         <>
-            <nav aria-label="Page navigation example">
-                <ul className="pagination justify-content-center">
-                    {pagination.prev !== null && (
+            <nav aria-label="Page navigation example" className="mb-3">
+                <div className="d-flex justify-content-center align-items-center gap-3 flex-wrap">
+                    <ul className="pagination justify-content-center mb-0">
                         <li className="page-item">
                             <button
-                                className="page-link"
+                                className={getDisabledStyle(pagination.current === pagination.first)}
+                                onClick={(e) => changePageNumberAndSize(e, pagination.first, pageSize)}>
+                                {'<<'}
+                            </button>
+                        </li>
+                        <li className="page-item">
+                            <button
+                                className={getDisabledStyle(pagination.prev === null)}
                                 onClick={(e) => changePageNumberAndSize(e, pagination.prev, pageSize)}>
-                                Previous
+                                {'<'}
                             </button>
                         </li>
-                    )}
-                    {pagination.current !== pagination.first && (
-                        <>
-                            <li className="page-item">
-                                <button
-                                    className="page-link"
-                                    onClick={(e) => changePageNumberAndSize(e, pagination.first, pageSize)}>
-                                    {pagination.first}
-                                </button>
-                            </li>
-                            <li className="page-item disabled">
-                                <button className="page-link">{'...'}</button>
-                            </li>
-                        </>
-                    )}
 
-                    {pagination.prev !== null && (
-                        <li className="page-item">
-                            <button
-                                className="page-link"
-                                onClick={(e) => changePageNumberAndSize(e, pagination.prev, pageSize)}>
-                                {pagination.prev}
-                            </button>
+                        {[...Array(pageBuffor)]
+                            .map((_, i) => i + 1)
+                            .reverse()
+                            .map((num) => {
+                                const pageBefore = pagination.current - num;
+                                if (pageBefore >= pagination.first) {
+                                    return (
+                                        <li key={num} className="page-item">
+                                            <button
+                                                className="page-link"
+                                                onClick={(e) => changePageNumberAndSize(e, pageBefore, pageSize)}>
+                                                {pageBefore}
+                                            </button>
+                                        </li>
+                                    );
+                                }
+                            })}
+
+                        <li className="page-item active">
+                            <button className="page-link">{pagination.current}</button>
                         </li>
-                    )}
 
-                    <li className="page-item active">
-                        <button
-                            className={`page-link ${pagination.current === pagination.first || pagination.current === pagination.last ? 'disabled' : ''}`}
-                            onClick={(e) => changePageNumberAndSize(e, pagination.first, pageSize)}>
-                            {pagination.current}
-                        </button>
-                    </li>
-
-                    {pagination.next !== null && (
-                        <li className="page-item">
-                            <button
-                                className="page-link"
-                                onClick={(e) => changePageNumberAndSize(e, pagination.next, pageSize)}>
-                                {pagination.next}
-                            </button>
-                        </li>
-                    )}
-
-                    {pagination.current !== pagination.last && (
-                        <>
-                            <li className="page-item disabled">
-                                <button className="page-link">{'...'}</button>
-                            </li>
-                            <li className="page-item">
-                                <button
-                                    className="page-link"
-                                    onClick={(e) => changePageNumberAndSize(e, pagination.last, pageSize)}>
-                                    {pagination.last}
-                                </button>
-                            </li>
-                        </>
-                    )}
-                    {pagination.next !== null && (
-                        <li className="page-item">
-                            <button
-                                className="page-link"
-                                onClick={(e) => changePageNumberAndSize(e, pagination.next, pageSize)}>
-                                Next
-                            </button>
-                        </li>
-                    )}
-                    <li>
-                        <div className="btn-group ms-2">
-                            <button
-                                type="button"
-                                className="btn btn-outline-primary dropdown-toggle"
-                                data-bs-toggle="dropdown"
-                                aria-expanded="false">
-                                Page size: {pageSize}
-                            </button>
-                            <ul className="dropdown-menu">
-                                {[5, 10, 25, 50].map((pageSize, index) => (
-                                    <li key={index}>
+                        {[...Array(pageBuffor)].map((_, i) => {
+                            const pageAfter = pagination.current + (i + 1);
+                            if (pageAfter <= pagination.last) {
+                                return (
+                                    <li key={i} className="page-item">
                                         <button
-                                            className="dropdown-item"
-                                            onClick={(e) => changePageNumberAndSize(e, pagination.first, pageSize)}>
-                                            Page size: {pageSize}
+                                            className="page-link"
+                                            onClick={(e) => changePageNumberAndSize(e, pageAfter, pageSize)}>
+                                            {pageAfter}
                                         </button>
                                     </li>
-                                ))}
-                            </ul>
-                        </div>
-                    </li>
-                </ul>
+                                );
+                            }
+                        })}
+                        <li className="page-item">
+                            <button
+                                className={getDisabledStyle(pagination.next === null)}
+                                onClick={(e) => changePageNumberAndSize(e, pagination.next, pageSize)}>
+                                {'>'}
+                            </button>
+                        </li>
+                        <li className="page-item">
+                            <button
+                                className={getDisabledStyle(pagination.current === pagination.last)}
+                                onClick={(e) => changePageNumberAndSize(e, pagination.last, pageSize)}>
+                                {'>>'} {pagination.last}
+                            </button>
+                        </li>
+                    </ul>
+                    <div className="btn-group">
+                        <button
+                            type="button"
+                            className="btn btn-outline-primary dropdown-toggle"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false">
+                            Page size: {pageSize}
+                        </button>
+                        <ul className="dropdown-menu">
+                            {[5, 10, 25, 50].map((pageSize, index) => (
+                                <li key={index}>
+                                    <button
+                                        className="dropdown-item"
+                                        onClick={(e) => changePageNumberAndSize(e, pagination.first, pageSize)}>
+                                        Page size: {pageSize}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
             </nav>
         </>
     );
 };
+
+function getDisabledStyle(condition: boolean): string {
+    return condition ? 'page-link disabled' : 'page-link ';
+}
