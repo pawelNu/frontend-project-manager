5176aa191075a2d506f76e56fb4425307ac265f6
pawelNu <pnurzynski17@gmail.com>
2025-04-21 17:37:17 +0200

    style: code format

--- src/components/common/DataTable/types.ts
+++ src/components/common/DataTable/types.ts
@@ -1,40 +1,36 @@
-
 export type Column<T> = {
     accessor: keyof T | string;
     label: string;
     sortable?: boolean;
     render?: (row: T) => React.ReactNode;
-  };
-  
-  export type FilterOption = { label: string; value: string | number | boolean };
-  
-  export type FilterConfig = {
+};
+
+export type FilterOption = { label: string; value: string | number | boolean };
+
+export type FilterConfig = {
     accessor: string;
     label?: string;
     type: 'text' | 'select' | 'checkbox' | 'number';
     options?: FilterOption[];
-  };
-  
-  export type SortState = {
+};
+
+export type SortState = {
     field: string | null;
     direction: 'asc' | 'desc' | null;
-  };
-  
+};
 
 export type TableParams<F = Record<string, unknown>> = {
     pageNumber: number;
     pageSize: number;
     filters: F;
     sort: SortState;
-  };
-  
-  
-  export type PaginationType = {
+};
+
+export type PaginationType = {
     pageNumber: number;
     pageSize: number;
     totalPages: number;
     totalElements: number;
     hasPrevious: boolean;
     hasNext: boolean;
-  };
-  
+};
