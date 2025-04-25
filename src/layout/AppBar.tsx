2774c7d4810f2a1a0757ac4dfdf3ee3feec7a41b
pawelNu <pnurzynski17@gmail.com>
2025-04-25 17:09:30 +0200

    feat: added companies full crud

---
+++ src/layout/AppBar.tsx
@@ -0,0 +1,17 @@
+import { AppBar, TitlePortal } from "react-admin";
+import { Box, useMediaQuery, Theme } from "@mui/material";
+
+
+import { AppBarToolbar } from "./AppBarToolbar";
+import { Logo } from "./Logo";
+
+export const CustomAppBar = () => {
+    const isLargeEnough = useMediaQuery<Theme>((theme) => theme.breakpoints.up("sm"));
+    return (
+        <AppBar color="secondary" toolbar={<AppBarToolbar />}>
+            <TitlePortal />
+            {isLargeEnough && <Logo />}
+            {isLargeEnough && <Box component="span" sx={{ flex: 1 }} />}
+        </AppBar>
+    );
+};
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Layout } from 'react-admin';
+
+
+import { CustomAppBar } from './AppBar';
+import { Menu } from './Menu';
+
+export const CustomLayout= ({ children }: { children: React.ReactNode }) => (
+    <Layout appBar={CustomAppBar} menu={Menu}>
+        {children}
+    </Layout>
+);
