8f5d590311999ca6a3768b96e56ded5bfe477e9a
pawelNu <pnurzynski17@gmail.com>
2025-07-31 09:54:46 +0200

    feat: added project step show

--- src/pages/project/step/ProjectStepShow.tsx
+++ src/pages/project/step/ProjectStepShow.tsx
@@ -13,11 +13,11 @@ export const ProjectStepShow = () => {
     return (
         <Show title={<ProjectStepShowTitle />} actions={<ShowActions />} queryOptions={{ onError }}>
             <SimpleShowLayout>
-                <TextField source="name" label="Project Name" />
-                <TextField source="categoryValue" label="Category" />
-                <TextField source="companyName" label="Company" />
-                <TextField source="assignedEmployee" label="Employee" />
+                <TextField source="name" label="Project Step Name" />
                 <TextField source="priorityValue" label="Priority" />
+                <TextField source="projectName" label="Project" />
+                <TextField source="assignedEmployee" label="Employee" />
+                <TextField source="deadline" label="Deadline" />
             </SimpleShowLayout>
         </Show>
     );
